63cefceb6dbef3c75d8e844a70bd42f76ab69a9d
Senyz1 <92756128+Senyz1@users.noreply.github.com>
2023-09-25 20:26:32 +0300

    Add project files.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // введення вихідних даних
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c=");
+            double c = double.Parse(Console.ReadLine());
+            // реалізація алгоритму розв'язання задачі
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            // вивід результату
+            Console.WriteLine("s={0}", s);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // введення вихідних даних
+            Console.Write("c=");
+            double c = double.Parse(Console.ReadLine());
+            Console.Write("h=");
+            double h = double.Parse(Console.ReadLine());
+            // реалізація алгоритму розв'язання задачі
+            double s = 0.5*c*h;
+            // вивід результату
+            Console.WriteLine("s={0}", s);
+            Console.ReadKey();
+        }
+    }
+}
